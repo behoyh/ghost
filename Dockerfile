9ac217956683d232953d9d6947e65f613b988b11
behoyh <beshoyhanna@outlook.com>
2022-05-11 09:51:20 -0400

    Update dockerfile and start script to ghost-cli

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,42 @@
 # Pull base image.
 FROM node:16
+ARG GHOST_VERSION=4.47.1
+
+# Install sudo
+RUN apt update && \
+    apt install sudo -y
+
+# Set ghost install user.
+RUN useradd ghost-user --home /ghost --password '' && \
+    usermod -aG sudo ghost-user
+USER ghost-user
+
+# Define working directory.
+WORKDIR /ghost
 
 # Install Ghost
-RUN \
-  cd /tmp && \
-  wget https://ghost.org/zip/ghost-latest.zip && \
-  unzip ghost-latest.zip -d /ghost && \
-  rm -f ghost-latest.zip && \
-  cd /ghost && \
-  npm install --production
+RUN echo '' | sudo -S npm install ghost-cli@latest -g && \
+    ghost install --db=sqlite3 --url http://mysite.com --no-prompt --no-stack --no-setup --no-start
 
 # Install Google Drive Integration
-RUN cd /ghost && \
-    mkdir content/adapters/storage && \
-    cd /ghost/content/adapters/storage && \
+RUN mkdir /ghost/versions/"$GHOST_VERSION"/content/adapters/storage && \
+    cd /ghost/versions/"$GHOST_VERSION"/content/adapters/storage && \
     git clone https://github.com/robincsamuel/ghost-google-drive.git && \
     cd ghost-google-drive && \
     npm install
-
-RUN useradd ghost --home /ghost
 
 # Add files.
 ADD start.bash /ghost-start
 
 # Set environment variables.
 ENV NODE_ENV development
+ENV GHOST_VERSION $GHOST_VERSION
 
 # Copy to override.
 COPY ./data /ghost-override
 
-# Define working directory.
-WORKDIR /ghost
+#Set ghost-start user
+USER root
 
 # Define default command.
 CMD ["bash", "/ghost-start"]
--- start.bash
+++ start.bash
@@ -4,6 +4,7 @@ GHOST="/ghost"
 OVERRIDE="/ghost-override"
 
 CONFIG="config.$NODE_ENV.json"
+VERSION="versions/$GHOST_VERSION"
 DATA="content/data"
 SETTINGS="content/settings"
 IMAGES="content/images"
@@ -11,39 +12,39 @@ THEMES="content/themes"
 
 cd "$GHOST"
 
-# Symlink data directory.
+echo "Symlink data directory."
 mkdir -p "$OVERRIDE/$DATA"
-rm -fr "$DATA"
-ln -s "$OVERRIDE/$DATA" "content"
+rm -fr "$VERSION/$DATA"
+ln -s "$OVERRIDE/$DATA" "$VERSION/content"
 
-# Symlink settings directory.
+echo "Symlink settings directory."
 mkdir -p "$OVERRIDE/$SETTINGS"
-rm -fr "$SETTINGS"
-ln -s "$OVERRIDE/$SETTINGS" "$SETTINGS"
+rm -fr "$VERSION/$SETTINGS"
+ln -s "$OVERRIDE/$SETTINGS" "$VERSION/$SETTINGS"
 
-# Symlink images directory.
+echo "Symlink images directory."
 mkdir -p "$OVERRIDE/$IMAGES"
-rm -fr "$IMAGES"
-ln -s "$OVERRIDE/$IMAGES" "$IMAGES"
+rm -fr "$VERSION/$IMAGES"
+ln -s "$OVERRIDE/$IMAGES" "$VERSION/$IMAGES"
 
-# Symlink config file.
+echo "Symlink config file."
 if [[ -f "$OVERRIDE/$CONFIG" ]]; then
   rm -f "$CONFIG"
   ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
 fi
 
-# Symlink themes.
+echo "Symlink themes."
 if [[ -d "$OVERRIDE/$THEMES" ]]; then
   for theme in $(find "$OVERRIDE/$THEMES" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
   do
-    rm -fr "$THEMES/$theme"
-    ln -s "$OVERRIDE/$THEMES/$theme" "$THEMES/$theme"
+    rm -fr "$VERSION/$THEMES/$theme"
+    ln -s "$OVERRIDE/$THEMES/$theme" "$VERSION/$THEMES/$theme"
   done
 fi
 
-# Start Ghost
-chown -R ghost:ghost /ghost /ghost-override
-su ghost << EOF
+echo "Starting Ghost..."
+chown -R ghost-user:ghost-user /ghost /ghost-override
+su ghost-user << EOF
 cd "$GHOST"
-NODE_ENV=${NODE_ENV:-production} npm start
+NODE_ENV=${NODE_ENV:-production} node "$VERSION"/index.js
 EOF
